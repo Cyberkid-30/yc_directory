cedb698927d64ea639dc05c25980f6d82dd1b7cb
Jan Phandoh <janphandoh@gmail.com>
2025-04-22 15:47:20 +0000

    built startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 interface Props {
   searchParams: Promise<{ query?: string }>;
@@ -6,6 +7,19 @@ interface Props {
 
 export default async function Home({ searchParams }: Props) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Kwabena" },
+      _id: 1,
+      description: "This is a description",
+      image: "/robot.jpg",
+      category: "Robots",
+      title: "We robots",
+    },
+  ];
 
   return (
     <>
@@ -19,6 +33,20 @@ export default async function Home({ searchParams }: Props) {
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post) => <StartupCard key={post._id} post={post} />)
+          ) : (
+            <p className="no-result">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
